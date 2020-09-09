3ac7cf3adc0e26f177c57384de27c2d827f1d61e
Davi <davigiordano2704@gmail.com>
2020-09-09 09:44:29 -0300

    Melhorar responsividade

--- script/buscarUsuario.js
+++ script/buscarUsuario.js
@@ -1,8 +1,10 @@
 buscarInput = document.getElementById("buscar");
-
+seuFeed = document.getElementById("seuFeed");
 buscarInput.addEventListener("input", function(){
     if(buscarInput.value != ""){
+        seuFeed.innerHTML = "Resultados da busca";
         listaPius.forEach(objetos => {
+            
             if(objetos.username.indexOf(buscarInput.value) > -1){
                 document.getElementById(objetos.username).style.display = "block";
                 document.getElementById(objetos.username).style.border = "2px solid #003F88";
@@ -13,6 +15,9 @@ buscarInput.addEventListener("input", function(){
     }
     
     else if(buscarInput.value == "") {
+        seuFeed.innerHTML = "Seu Feed";
+       // pinBtn.style.display = "block";
+
         listaPius.forEach(objetos => {
             document.getElementById(objetos.username).style.display = "block";
             document.getElementById(objetos.username).style.border = "none";
